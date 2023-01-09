ddd9669b2d6c59e8d08978db11dc68243718ca35
gltknkskn <59214160+gltknkskn@users.noreply.github.com>
2023-01-09 00:00:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-import streamlit as st
+import streamlit
 
-st.title('My Parents New Healthy Diner')
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale Spinach ...')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
